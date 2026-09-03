f6fac1eca2782ee09431d38c4d5348c8e1875224
YashikKhunt <yashikkhunt2002@gmail.com>
2026-09-03 10:49:41 +0200

    Initial Commit

---
+++ proximiPlayTests/proximiPlayTests.swift
@@ -0,0 +1,19 @@
+//
+//  proximiPlayTests.swift
+//  proximiPlayTests
+//
+//  Created by Yashik Khunt on 03.09.26.
+//
+
+import Testing
+@testable import proximiPlay
+
+struct proximiPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
